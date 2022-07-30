9f477d2d989634c2aa1ad733cf03eb798885783f
Ivan Stolov <ivan_stolov@mail.ru>
2022-07-30 14:44:18 +0400

    added entry point and tests with JUnit5

--- maven-test/src/test/java/com/github/klubidorka/AppTest.java
+++ maven-test/src/test/java/com/github/klubidorka/AppTest.java
@@ -1,20 +1,77 @@
 package com.github.klubidorka;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
+import org.assertj.core.api.Assertions;
 
-import org.junit.Test;
+import java.util.Arrays;
+import java.util.List;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+public class AppTest {
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    void helloWorldTest() {
+        App.main(new String[]{});
+    }
+
+    @Test
+    void helloTest() {
+        Assertions.assertThat(2 + 2).isEqualTo(4);
+    }
+
+    @Test
+    void strTest() {
+        String a = "Hello, world!";
+        String b = "Hello, world!";
+        Assertions.assertThat(a).isEqualTo(b);
+    }
+
+    @Test
+    void otherStrTest() {
+        String a = "Hello, world!";
+        Assertions.assertThat(a)
+                .startsWith("Hello")
+                .contains("llo, ")
+                .endsWith("!");
+    }
+
+    @BeforeAll
+    static void setUp() {
+        System.out.println("Testing started");
+    }
+
+    @AfterAll
+    static void Finalize() {
+        System.out.println("Testing finished");
+    }
+
+    @BeforeEach
+    void before() {
+        System.out.println("This method is called before each test");
+    }
+
+    @AfterEach
+    void after() {
+        System.out.println("This method is called after each test");
+    }
+
+    @Test
+    void exceptionTest() {
+        assertThatThrownBy(() -> {
+            List<String> list = Arrays.asList("String one", "String two");
+            list.get(2);
+        }).isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageContaining("Index 2 out of bounds for length 2");
+    }
+
+    @RepeatedTest(5)
+    void repeatedTest() {
+        System.out.println("You will see this message five times");
     }
 }
