20df6603385b224d86f291e957c2201b591383c2
Ivan Stolov <ivan_stolov@mail.ru>
2022-07-30 15:29:01 +0400

    maven exec plugin

--- maven-test/src/main/java/com/github/klubidorka/App.java
+++ maven-test/src/main/java/com/github/klubidorka/App.java
@@ -6,6 +6,7 @@ package com.github.klubidorka;
  */
 public class App {
     public static void main(String[] args) {
+        System.out.println("Hello World");
         for (String arg : args) {
             System.out.println(arg);
         }
